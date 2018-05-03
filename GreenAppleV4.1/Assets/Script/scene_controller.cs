2824d087b65c85bb77f18641d87ff8f5b6947f03
AaronCur <C00202505@itcarlow.ie>
2018-05-03 12:06:00 +0100

    Updated

---
+++ GreenAppleV4.1/Assets/Script/scene_controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Loads scene with index of 1
+    /// The main Game scene
+    /// Public function accessed from
+    /// UI button OnClick() event.
+    /// </summary>
+    public void GoToGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    /// <summary>
+    /// Quits the current Unity application
+    /// closing the .exe
+    /// Public function accessed from UI
+    /// function OnClick event.
+    /// </summary>
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
